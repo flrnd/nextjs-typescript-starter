1da328d609200ed1e71aa52b12d8cd21fdc4425d
Florian Rand <florian.rand@gmail.com>
2021-04-28 18:06:20 +0200

    Rename works to projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,12 @@
 import Layout from '../components/layout/layout'
 import Container from '../components/layout/container'
 
-const Works = (): JSX.Element => (
+const Projects = (): JSX.Element => (
   <Layout>
     <Container>
-      <h1>Works page</h1>
+      <h1>Projects page</h1>
     </Container>
   </Layout>
 )
 
-export default Works
+export default Projects
