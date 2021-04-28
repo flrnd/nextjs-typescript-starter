0a4cefc4f70c633211f0a4d57b1af2960e509eca
Florian Rand <florian.rand@gmail.com>
2021-04-28 16:49:57 +0200

    Clean white space

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,7 +5,7 @@ const navMenuItems = siteConfig.navMenu
 
 const Header = (): JSX.Element => {
   return (
-    <div className="page-header h-12 md:h-16 fixed w-full z-0 top-0 bg-white  ">
+    <div className="page-header h-12 md:h-16 fixed w-full z-0 top-0 bg-white">
       <Nav menu={navMenuItems} />
     </div>
   )
