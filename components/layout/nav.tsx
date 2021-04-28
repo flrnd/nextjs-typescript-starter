9b052514796c04f81605198ad24f3caa4d74d680
Florian Rand <florian.rand@gmail.com>
2021-04-28 14:26:49 +0200

    Add link to logo

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useState } from 'react'
 import { Menu } from '../../lib/interfaces'
 import NavMenu from './nav-menu'
@@ -13,7 +14,9 @@ const Nav = ({ menu }: Props): JSX.Element => {
     <nav className="w-full z-1 bg-white">
       <div className="w-full mx-auto flex flex-wrap items-center justify-between mt-0 py-2 px-3">
         <div className="flex items-center">
-          <span className="text-4xl font-bold">logo</span>
+          <Link href="/">
+            <a className="text-4xl font-bold">logo</a>
+          </Link>
         </div>
 
         <div className="block md:hidden pr-4">
