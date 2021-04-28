39ec9ca9c2f759a00461c1094cb9fb08c2c045ca
Florian Rand <florian.rand@gmail.com>
2021-04-28 17:20:17 +0200

    Cover -> title -> author -> date

--- components/post/post-header.tsx
+++ components/post/post-header.tsx
@@ -10,13 +10,12 @@ interface Props {
 const PostHeader = ({ title, coverImage, date }: Props): JSX.Element => {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">author</div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">author</div>
+        <PostTitle>{title}</PostTitle>
+        <div className="block mb-6">author</div>
         <div className="mb-6 text-lg">{date}</div>
       </div>
     </>
