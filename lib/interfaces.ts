8439ea2be73e7b795a9a1e9c2b24d23a46c013aa
Florian Rand <florian.rand@gmail.com>
2021-04-28 15:22:41 +0200

    Add interface items and rename Post

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -2,3 +2,19 @@ export interface Menu {
   name: string
   link: string
 }
+
+export interface Items {
+  [key: string]: string
+}
+
+export interface IPost {
+  slug: string
+  title: string
+  date: string
+  coverImage: string
+  description: string
+  ogImage: {
+    url: string
+  }
+  content: string
+}
