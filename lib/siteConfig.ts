8e6a753a7a76dd442e3788e84f1fc8ce0c849250
Florian Rand <florian.rand@gmail.com>
2021-04-28 18:16:40 +0200

    Add more siteConfig placeholder data

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -1,27 +1,63 @@
 import { getYear } from './util'
 
-const brand = 'brand'
-const metaDescription = ''
-const canonicalURL = 'http://www.example.com'
+// Company data
+const company = {
+  name: 'company name',
+  description: 'The best company & Co',
+  brand: 'brand name',
+  canonicalURL: 'http://www.example.com',
+  address: '908 New Hampshire Avenue #100, Washington, DC 20037, United States',
+  phone: '+1 123-123-2345',
+  email: 'info@example.com',
+}
 
-const siteConfig = {
-  SEO: {
-    brand: brand,
-    metaDescription: metaDescription,
-    canonicalURL: canonicalURL,
-    pages: {
-      hero: {
-        title: 'lorem ipsum',
-      },
-    },
+// Social media links
+const social = {
+  twitter: '@twitter',
+  instagram: '@instagram',
+  facebook: '@facebook',
+  youtube: '@youtube',
+  linkedin: '@linkedin',
+}
+
+// Data for pages SEO
+const pages = {
+  hero: {
+    title: 'lorem ipsum',
+    description: '',
   },
+  about: {
+    title: 'about us',
+    description: '',
+  },
+  projects: {
+    title: 'My awesome projects',
+    description: '',
+  },
+  blog: {
+    title: 'my awesome blog',
+    description: '',
+  },
+}
+
+// site object
+const siteConfig = {
+  seo: {
+    brand: company.brand,
+    description: company.description,
+    canonicalURL: company.canonicalURL,
+    pages: pages,
+  },
+  social: social,
   navMenu: [
-    { name: 'works', link: 'works' },
-    { name: 'about me', link: 'about' },
+    { name: 'Our work', link: 'projects' },
+    { name: 'about us', link: 'about' },
     { name: 'blog', link: 'posts' },
   ],
   footerMenu: [{ name: 'Privacy', link: 'privacy' }],
-  footerCopyright: `Copyright ${brand} ©${getYear()} , All rights reserved.`,
+  footerCopyright: `Copyright ${
+    company.brand
+  } ©${getYear()}, All rights reserved.`,
 }
 
 export default siteConfig
